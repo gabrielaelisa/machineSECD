e7b05bdaafd79c79a70f5507adfb5cf4f947df29
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2018-06-21 23:47:43 -0400

    adding example ARM code

---
+++ test1.s
@@ -0,0 +1,23 @@
+.text
+.global main
+.extern printf
+main:
+     stmfd sp!, {lr}
+     ldr r0, =string
+     mov r1, #1
+     stmfd r13!, {r1}
+     mov r1, #2
+     stmfd r13!, {r1}
+     mov r1, #3
+     stmfd r13!, {r1}
+     ldmfd r13!,{r2,r3}
+     add r1, r2, r3 
+     stmfd r13!,{r1}
+     ldmfd r13!,{r2,r3}
+     add r1, r2, r3 
+     stmfd r13!,{r1}
+     ldmfd r13!, {r1}
+     bl printf
+     ldmfd sp!, {pc}
+.data
+string: .asciz "%d\n"
